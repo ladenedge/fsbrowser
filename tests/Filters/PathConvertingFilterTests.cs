1f159e7481d71e871e15846bbd1385c865ffbe21
Jay Miller <jnmiller@cryptofreak.org>
2018-04-12 01:59:45 -0600

    Added sufficient unit test coverage.

---
+++ tests/Filters/PathConvertingFilterTests.cs
@@ -0,0 +1,82 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using FSBrowser.Filters;
+using NUnit.Framework;
+
+namespace FSBrowser.Models.Tests
+{
+    public class PathConvertingFilterTests
+    {
+        TestConvertingFilter Filter { get; set; }
+
+        [SetUp]
+        public void Init()
+        {
+            var fs = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                [@"c:\home\file.txt"] = new MockFileData("content"),
+                [@"c:\home\dir\file.jpg"] = new MockFileData("content")
+            });
+            Filter = new TestConvertingFilter(fs);
+        }
+
+        [Test]
+        public void AdjustPath_ThrowsOnUnexpectedType()
+        {
+            Assert.That(() => Filter.TestAdjustPath(@"c:\home\file.txt", typeof(DateTime)), Throws.InstanceOf<KeyNotFoundException>());
+        }
+
+        [Test]
+        public void AdjustPath_ReturnsExpectedType()
+        {
+            var result = Filter.TestAdjustPath(@"c:\home\file.txt", typeof(FileInfoBase));
+            Assert.That(result, Is.InstanceOf<FileInfoBase>());
+        }
+        
+        [Test]
+        public void AdjustPath_ConvertsFile()
+        {
+            var path = @"c:\home\file.txt";
+            var result = Filter.TestAdjustPath(path, typeof(FileInfoBase)) as FileInfoBase;
+            Assert.That(result.FullName, Is.EqualTo(path));
+        }
+
+        [Test]
+        public void AdjustPath_ConvertsDir()
+        {
+            var path = @"c:\home\dir";
+            var result = Filter.TestAdjustPath(path, typeof(DirectoryInfoBase)) as DirectoryInfoBase;
+            Assert.That(result.FullName, Is.EqualTo(path));
+        }
+
+        [Test]
+        public void AdjustPath_ConvertsFileFromOnlyPath()
+        {
+            var path = @"c:\home\file.txt";
+            var result = Filter.TestAdjustPath(path, typeof(FileSystemInfoBase)) as FileSystemInfoBase;
+            Assert.That(result.FullName, Is.EqualTo(path));
+        }
+
+        [Test]
+        public void AdjustPath_ConvertsDirFromOnlyPath()
+        {
+            var path = @"c:\home\dir";
+            var result = Filter.TestAdjustPath(path, typeof(FileSystemInfoBase)) as FileSystemInfoBase;
+            Assert.That(result.FullName, Is.EqualTo(path));
+        }
+
+        class TestConvertingFilter : PathConvertingFilter
+        {
+            public TestConvertingFilter(IFileSystem fs) : base(fs) { }
+
+            public object TestAdjustPath(string path, Type intendedType)
+            {
+                return base.AdjustPath(path, intendedType);
+            }
+        }
+    }
+}
---
+++ tests/Filters/PathRootingFilterTests.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+using System.Linq;
+using FSBrowser.Filters;
+using Moq;
+using NUnit.Framework;
+
+namespace FSBrowser.Models.Tests
+{
+    public class PathRootingFilterTests
+    {
+        TestRootingFilter Filter { get; set; }
+
+        [SetUp]
+        public void Init()
+        {
+            var fs = new MockFileSystem();
+            var config = Mock.Of<IConfig>();
+            Mock.Get(config).SetupGet(c => c.HomeDirectory).Returns(@"c:\home");
+
+            Filter = new TestRootingFilter(config, fs);
+        }
+
+        [Test]
+        public void AdjustPath_ThrowsWhenPathIsRootedToNonHomeDirectory()
+        {
+            Assert.That(() => Filter.TestAdjustPath(@"c:\temp", null), Throws.ArgumentException);
+        }
+
+        [Test]
+        public void AdjustPath_DoesNotChangeHomedPath()
+        {
+            var expected = @"c:\home\dir";
+            var result = Filter.TestAdjustPath(expected, null);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void AdjustPath_ConcatsHomeDirectory()
+        {
+            var result = Filter.TestAdjustPath(@"test\file", null);
+            Assert.That(result, Is.EqualTo(@"c:\home\test\file"));
+        }
+
+        [Test]
+        public void AdjustPath_FactorsOutParentDots()
+        {
+            var result = Filter.TestAdjustPath(@"test\..\test\..\test\file", null);
+            Assert.That(result, Is.EqualTo(@"c:\home\test\file"));
+        }
+
+        [Test]
+        public void AdjustPath_DisallowsLeavingJail()
+        {
+            var result = Filter.TestAdjustPath(@"..\file", null);
+            Assert.That(result, Is.EqualTo(@"c:\home"));
+        }
+
+        class TestRootingFilter : PathRootingFilter
+        {
+            public TestRootingFilter(IConfig config, IFileSystem fs) : base(config, fs) { }
+
+            public object TestAdjustPath(string path, Type intendedType)
+            {
+                return base.AdjustPath(path, intendedType);
+            }
+        }
+    }
+}
